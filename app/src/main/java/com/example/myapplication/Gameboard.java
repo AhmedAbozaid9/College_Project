de0b8d63126c41c656733079a3b3421ed95e1a8b
Ayman Adel <aymanadel841@gmail.com>
2022-12-17 18:28:56 +0200

    setBoard Function

--- app/src/main/java/com/example/myapplication/Gameboard.java
+++ app/src/main/java/com/example/myapplication/Gameboard.java
@@ -6,28 +6,28 @@ import android.graphics.drawable.Drawable;
 import android.graphics.drawable.Icon;
 import android.media.Image;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
-import java.util.Arrays;
 import java.util.Objects;
 
 public class Gameboard extends AppCompatActivity {
 
-    public String[] gameBoard = new String[9];
+    public String[] scoreBoard = new String[9];
     Boolean isPlayer1 = true;
-    // container
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.d("a7a", Arrays.toString(gameBoard));
+
         ImageView imageview;
         super.onCreate(savedInstanceState);
         //hiding the title bar
@@ -35,7 +35,8 @@ public class Gameboard extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).hide();
 
         setContentView(R.layout.activity_gameboard);
-        renderBoard();
+
+       setBoard();
 
         Button returnBtn = (Button) findViewById(R.id.button_return);
         returnBtn.setOnClickListener(new View.OnClickListener() {
@@ -47,40 +48,58 @@ public class Gameboard extends AppCompatActivity {
         });
 
 
+
+
     }
 
-    public void renderBoard(String[] board) {
-
-        // clear the container
-
-
-        for(int i = 0; i < board.length;i++){
-            if(board[i] == null) {
-                //render an empty button
-            }
-            else if(board[i] == "x"){
-                //render button with x
-            }
-            else if(board[i] == "0") {
-                //render button with o
-            }
-        }
-    }
     public void handleClick(ImageButton btn,int idx) {
-        // "x" "o" "draw"
-        String status = checkBoard(gameBoard);
 
         if (isPlayer1) {
-            gameBoard[idx] = "x";
+            scoreBoard[idx] = "x";
             btn.setImageResource(R.drawable.x_icon);
         } else {
-            gameBoard[idx] = "o";
+            scoreBoard[idx] = "o";
             btn.setImageResource(R.drawable.o_icon);
         }
+
+    }
+    public void setBoard(){
+        LinearLayout l1 = (LinearLayout) findViewById(R.id.linearLayout1);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT
+                ,ViewGroup.LayoutParams.MATCH_PARENT));
+        params.weight = 1.0f;
+        ImageButton b0 = new ImageButton(this);
+        b0.setLayoutParams(params);
+        l1.addView(b0);
+        ImageButton b1 = new ImageButton(this);
+        b1.setLayoutParams(params);
+        l1.addView(b1);
+        ImageButton b2 = new ImageButton(this);
+        b2.setLayoutParams(params);
+        l1.addView(b2);
+        LinearLayout l2 = (LinearLayout) findViewById(R.id.linearLayout2);
+        ImageButton b3 = new ImageButton(this);
+        b3.setLayoutParams(params);
+        l2.addView(b3);
+        ImageButton b4 = new ImageButton(this);
+        b4.setLayoutParams(params);
+        l2.addView(b4);
+        ImageButton b5 = new ImageButton(this);
+        b5.setLayoutParams(params);
+        l2.addView(b5);
+        LinearLayout l3 = (LinearLayout) findViewById(R.id.linearLayout3);
+        ImageButton b6 = new ImageButton(this);
+        b6.setLayoutParams(params);
+        l3.addView(b6);
+        ImageButton b7 = new ImageButton(this);
+        b7.setLayoutParams(params);
+        l3.addView(b7);
+        ImageButton b8 = new ImageButton(this);
+        b8.setLayoutParams(params);
+        l3.addView(b8);
     }
 
-    public String checkBoard(String[] board) {
-        return "";
+
     }
-}
+
 
