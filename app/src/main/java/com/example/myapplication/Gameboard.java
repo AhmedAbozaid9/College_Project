6200a9463bc795b4d7e0e76403e7f1aaff894926
Ahmed Abozaid <ahmedabozaid493@gmail.com>
2022-12-17 16:57:14 +0200

    fucked up the code again

--- app/src/main/java/com/example/myapplication/Gameboard.java
+++ app/src/main/java/com/example/myapplication/Gameboard.java
@@ -6,6 +6,7 @@ import android.graphics.drawable.Drawable;
 import android.graphics.drawable.Icon;
 import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
@@ -15,16 +16,18 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.util.Arrays;
 import java.util.Objects;
 
 public class Gameboard extends AppCompatActivity {
 
-    public String[] scoreBoard = new String[9];
+    public String[] gameBoard = new String[9];
     Boolean isPlayer1 = true;
+    // container
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
+        Log.d("a7a", Arrays.toString(gameBoard));
         ImageView imageview;
         super.onCreate(savedInstanceState);
         //hiding the title bar
@@ -32,6 +35,7 @@ public class Gameboard extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).hide();
 
         setContentView(R.layout.activity_gameboard);
+        renderBoard();
 
         Button returnBtn = (Button) findViewById(R.id.button_return);
         returnBtn.setOnClickListener(new View.OnClickListener() {
@@ -41,122 +45,42 @@ public class Gameboard extends AppCompatActivity {
                 view.getContext().startActivity(intent);
             }
         });
-        ImageButton b0 = findViewById(R.id.imageButton00);
-        b0.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-               handleClick(b0,0);
-            }
-        });
-        ImageButton b1 = findViewById(R.id.imageButton01);
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                handleClick(b1,1);
-            }
-        });
-        ImageButton b2 = findViewById(R.id.imageButton02);
-        b2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-               handleClick(b2,2);
 
-            }
-        });
-        ImageButton b3 = findViewById(R.id.imageButton10);
-        b3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-               handleClick(b3,3);
-            }
-        });
-        ImageButton b4 = findViewById(R.id.imageButton11);
-        b4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                handleClick(b4,4);
-            }
-        });
-        ImageButton b5 = findViewById(R.id.imageButton12);
-        b5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                handleClick(b5,5);
-
-            }
-        });
-        ImageButton b6 = findViewById(R.id.imageButton20);
-        b6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                handleClick(b6,6);
-            }
-        });
-        ImageButton b7 = findViewById(R.id.imageButton21);
-        b7.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-               handleClick(b7,7);
-            }
-        });
-        ImageButton b8 = findViewById(R.id.imageButton22);
-        b8.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                handleClick(b8,8);
-            }
-        });
 
     }
 
+    public void renderBoard(String[] board) {
+
+        // clear the container
+
+
+        for(int i = 0; i < board.length;i++){
+            if(board[i] == null) {
+                //render an empty button
+            }
+            else if(board[i] == "x"){
+                //render button with x
+            }
+            else if(board[i] == "0") {
+                //render button with o
+            }
+        }
+    }
     public void handleClick(ImageButton btn,int idx) {
+        // "x" "o" "draw"
+        String status = checkBoard(gameBoard);
 
         if (isPlayer1) {
-            scoreBoard[idx] = "x";
+            gameBoard[idx] = "x";
             btn.setImageResource(R.drawable.x_icon);
         } else {
-            scoreBoard[idx] = "o";
+            gameBoard[idx] = "o";
             btn.setImageResource(R.drawable.o_icon);
         }
     }
-    private boolean checkForWin() {
-        String[][] field = new String[3][3];
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                field[i][j] = buttons[i][j].getText().toString();
-            }
-        }
-
-        for (int i = 0; i < 3; i++) {
-            if (field[i][0].equals(field[i][1])
-                    && field[i][0].equals(field[i][2])
-                    && !field[i][0].equals("")) {
-                return true;
-            }
-        }
-
-        for (int i = 0; i < 3; i++) {
-            if (field[0][i].equals(field[1][i])
-                    && field[0][i].equals(field[2][i])
-                    && !field[0][i].equals("")) {
-                return true;
-            }
-        }
-
-        if (field[0][0].equals(field[1][1])
-                && field[0][0].equals(field[2][2])
-                && !field[0][0].equals("")) {
-            return true;
-        }
-
-        if (field[0][2].equals(field[1][1])
-                && field[0][2].equals(field[2][0])
-                && !field[0][2].equals("")) {
-            return true;
-        }
-
-        return false;
+    public String checkBoard(String[] board) {
+        return "";
     }
 }
 
