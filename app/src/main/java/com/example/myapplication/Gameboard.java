d7969b0a6eda258f74b863fa900e235eb481e1b6
Ahmed Abozaid <ahmedabozaid493@gmail.com>
2022-12-17 16:19:11 +0200

    fucked up the code

--- app/src/main/java/com/example/myapplication/Gameboard.java
+++ app/src/main/java/com/example/myapplication/Gameboard.java
@@ -18,8 +18,13 @@ import org.w3c.dom.Text;
 import java.util.Objects;
 
 public class Gameboard extends AppCompatActivity {
+
+    public String[] scoreBoard = new String[9];
+    Boolean isPlayer1 = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         ImageView imageview;
         super.onCreate(savedInstanceState);
         //hiding the title bar
@@ -28,78 +33,130 @@ public class Gameboard extends AppCompatActivity {
 
         setContentView(R.layout.activity_gameboard);
 
-        Button breturn = (Button) findViewById(R.id.button_return);
-        breturn.setOnClickListener(new View.OnClickListener() {
+        Button returnBtn = (Button) findViewById(R.id.button_return);
+        returnBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(view.getContext(), Home.class);
                 view.getContext().startActivity(intent);
             }
         });
-        ImageButton b00 = findViewById(R.id.imageButton00);
-        b00.setOnClickListener(new View.OnClickListener() {
+        ImageButton b0 = findViewById(R.id.imageButton00);
+        b0.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                b00.setImageResource(R.drawable.x_icon);
+               handleClick(b0,0);
             }
         });
-        ImageButton b01 = findViewById(R.id.imageButton01);
-        b01.setOnClickListener(new View.OnClickListener() {
+        ImageButton b1 = findViewById(R.id.imageButton01);
+        b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                b01.setImageResource(R.drawable.x_icon);
+                handleClick(b1,1);
             }
         });
-        ImageButton b02 = findViewById(R.id.imageButton02);
-        b02.setOnClickListener(new View.OnClickListener() {
+        ImageButton b2 = findViewById(R.id.imageButton02);
+        b2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                b02.setImageResource(R.drawable.o_icon);
+               handleClick(b2,2);
+
             }
         });
-        ImageButton b10 = findViewById(R.id.imageButton10);
-        b10.setOnClickListener(new View.OnClickListener() {
+        ImageButton b3 = findViewById(R.id.imageButton10);
+        b3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                b10.setImageResource(R.drawable.o_icon);
+               handleClick(b3,3);
             }
         });
-        ImageButton b11 = findViewById(R.id.imageButton11);
-        b11.setOnClickListener(new View.OnClickListener() {
+        ImageButton b4 = findViewById(R.id.imageButton11);
+        b4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                b11.setImageResource(R.drawable.x_icon);
+                handleClick(b4,4);
             }
         });
-        ImageButton b12 = findViewById(R.id.imageButton12);
-        b12.setOnClickListener(new View.OnClickListener() {
+        ImageButton b5 = findViewById(R.id.imageButton12);
+        b5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                b12.setImageResource(R.drawable.o_icon);
+                handleClick(b5,5);
+
             }
         });
-        ImageButton b20 = findViewById(R.id.imageButton20);
-        b20.setOnClickListener(new View.OnClickListener() {
+        ImageButton b6 = findViewById(R.id.imageButton20);
+        b6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                b20.setImageResource(R.drawable.o_icon);
+                handleClick(b6,6);
             }
         });
-        ImageButton b21 = findViewById(R.id.imageButton21);
-        b21.setOnClickListener(new View.OnClickListener() {
+        ImageButton b7 = findViewById(R.id.imageButton21);
+        b7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                b21.setImageResource(R.drawable.o_icon);
+               handleClick(b7,7);
             }
         });
-        ImageButton b22 = findViewById(R.id.imageButton22);
-        b22.setOnClickListener(new View.OnClickListener() {
+        ImageButton b8 = findViewById(R.id.imageButton22);
+        b8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                b22.setImageResource(R.drawable.x_icon);
+                handleClick(b8,8);
             }
         });
 
+    }
+
+    public void handleClick(ImageButton btn,int idx) {
+
+        if (isPlayer1) {
+            scoreBoard[idx] = "x";
+            btn.setImageResource(R.drawable.x_icon);
+        } else {
+            scoreBoard[idx] = "o";
+            btn.setImageResource(R.drawable.o_icon);
+        }
+    }
+    private boolean checkForWin() {
+        String[][] field = new String[3][3];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                field[i][j] = buttons[i][j].getText().toString();
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if (field[i][0].equals(field[i][1])
+                    && field[i][0].equals(field[i][2])
+                    && !field[i][0].equals("")) {
+                return true;
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if (field[0][i].equals(field[1][i])
+                    && field[0][i].equals(field[2][i])
+                    && !field[0][i].equals("")) {
+                return true;
+            }
+        }
+
+        if (field[0][0].equals(field[1][1])
+                && field[0][0].equals(field[2][2])
+                && !field[0][0].equals("")) {
+            return true;
+        }
+
+        if (field[0][2].equals(field[1][1])
+                && field[0][2].equals(field[2][0])
+                && !field[0][2].equals("")) {
+            return true;
+        }
+
+        return false;
     }
 }
 
