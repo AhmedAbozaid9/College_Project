1600acb8c536d7a4f8e87db80350d5a4fc99e19c
Marwanemad <marawanemad717@gmail.com>
2022-12-21 19:00:13 +0200

    History

--- app/src/main/java/com/example/myapplication/History.java
+++ app/src/main/java/com/example/myapplication/History.java
@@ -2,20 +2,27 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ListView;
 
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class History extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);;
         //hiding the title bar
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         Objects.requireNonNull(getSupportActionBar()).hide();
